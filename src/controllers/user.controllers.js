a769bddab1d1dbe8641c23423750468150e750ee
Balvarazagit <211260116007setiit@gmail.com>
2026-04-06 03:38:32 -0700

    add register controller and bug fix

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,61 @@
 import { asyncHandler } from '../utilis/asyncHandler.js'
+import { ApiError } from '../utilis/ApiError.js'
+import { User } from '../models/user.models.js'
+import { uploadOnCloudinary } from '../utilis/cloudinary.js'
+import { ApiResponse } from '../utilis/ApiResponse.js'
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "ok"
+
+    const { fullName, email, password, userName } = req.body
+    console.log("Register User", fullName, email, password, userName)
+    if (
+        [fullName, email, password, userName].some(field => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [
+            { email },
+            { userName }
+        ]
     })
+    if (existedUser) {
+        throw new ApiError(409, "User already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar?.[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!avatar) {
+        throw new ApiError(400, "Failed to upload avatar")
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        userName: userName.toLowerCase()
+    })
+
+    const createdUSer = await User.findById(user._id).select("-password -refreshToken")
+
+    if (!createdUSer) {
+        throw new ApiError(500, "Failed to create user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUSer, "User created successfully")
+    )
 }
 )
 
